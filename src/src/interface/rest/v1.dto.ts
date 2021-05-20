858138d9318250b6ea2b310fa50cdc45597a3b2f
Danial Manavi <dmanavi@live.com>
2021-05-19 23:07:45 -0700

    Fix invalid class path.

--- src/src/interface/rest/v1.dto.ts
+++ src/src/interface/rest/v1.dto.ts
@@ -2,6 +2,6 @@
  * V1 DTO
  */
 
-import { SessionObject } from "src/shared/dto/session_object.dto";
+import { SessionObject } from '../../shared/dto/session_object.dto';
 
 export class SessionObjectHttpResponse extends SessionObject { }
