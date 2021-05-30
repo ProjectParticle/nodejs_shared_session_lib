a6a70c542e1750109cbf87bd7cadafd59bbede57
Danial Manavi <dmanavi@live.com>
2021-05-30 00:24:26 -0700

    Update shared lib.

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,11 +2,6 @@
  * Shared Session Service / Shared
  */
 
-export * from './event/session_created.event';
-export * from './event/session_destroyed.event';
-export * from './event/session_not_found.event';
-export * from './event/session_updated.event';
-
-export * from './session.model';
 export * from './session_data.type';
+export * from './session.model';
 export * from './storage_driver.type';
--- packages/shared/src/session.model.ts
+++ packages/shared/src/session.model.ts
@@ -9,5 +9,8 @@ export class SessionModel {
   constructor(
     readonly id: string,
     readonly data: SessionData,
+    readonly createdAt: Date,
+    readonly updatedAt: Date,
+    readonly expiresIn: number
   ) { }
 }
